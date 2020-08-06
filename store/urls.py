a1cb1eb3bf2922b0457fab3649121b4af6bbec4e
juliobriones <julesc00@hotmail.com>
2020-08-06 13:07:50 -0500

    Finished styling the checkout page.

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
-    path('checkout/<str:pk>/', views.store, name="store"),
+    path('checkout/', views.checkout, name="checkout"),
 
 ]
